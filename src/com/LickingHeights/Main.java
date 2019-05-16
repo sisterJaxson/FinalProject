f31d7cc2fd69e080527d8ddf2170b8f7f8cc127b
Unknown <jaxson.weaver.2022@student.lhschools.org>
2019-05-16 14:05:22 -0400

    Made game code

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -8,30 +8,48 @@ public class Main {
     public static void main(String[] args) {
 
         String userTurn;
-        String computerTurn ="";
+        String computerTurn = "";
         int computerInt;
         String response;
         Scanner keyboard;
         keyboard = new Scanner(System.in);
         Random variable = new Random();
         System.out.println("Would you like to play Alaska Roxxy Jinx?");
-        computerInt = variable.nextInt(3)+1;
 
+        keyboard.nextLine();
+        computerInt = variable.nextInt(3) + 1;
         if (computerInt == 1)
-            computerTurn = "Alaska";
+            computerTurn = "Alaska"; //R
 
-        else if(computerInt == 2)
-            computerTurn = "Roxxy";
+        else if (computerInt == 2)
+            computerTurn = "Roxxy"; //P
 
-        else if(computerInt == 3)
-            computerTurn = "Jinx";
+        else if (computerInt == 3)
+            computerTurn = "Jinx"; //S
 
         System.out.println("Alaska Roxxy Jinx shook:");
         userTurn = keyboard.next();
-        System.out.println("I pick: "+computerTurn);
+        System.out.println("I pick: " + computerTurn);
 
-        
+        if (userTurn == computerTurn)
+            System.out.println("Tie");
+        else if (userTurn == "Alaska")
+            if (computerTurn == "Jinx")
+                System.out.println("Alaska beats Jinx. You win!");
+            else if (userTurn == "Roxxy")
+                System.out.println("Roxxy beats Alaska. You loose :(");
+            else if (userTurn == "Roxxy")
+                if (computerTurn == "Jinx")
+                    System.out.println("Jinx beats Roxxy. You loose :(");
+                else if (userTurn == "Alaska")
+                    System.out.println("Roxxy beats Alaska. You win!");
+                else if (userTurn == "Jinx")
+                    if (computerTurn == "Roxxy")
+                        System.out.println("Jinx beats Roxxy. You win!");
+                    else if (computerTurn == "Alaska")
+                        System.out.println("Alaska beats Roxxy. You loose :(");
+                    else
+                        System.out.println("What u on??? wrong game!");
 
-
-
-}
+    }
+    }
